cd42dc631195ac6add1ea1794bd7af30757903e0
sulrus <ruslan@sulrus.ru>
2023-06-07 17:55:44 +0300

    init

---
+++ test/fixtures/fulfillment-order/res/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.locationsForMove = require('./locations-for-move');
+exports.fulfillments = require('./fulfillments');
+exports.cancel = require('./cancel');
+exports.close = require('./close');
+exports.list = require('./list');
+exports.move = require('./move');
+exports.get = require('./get');
